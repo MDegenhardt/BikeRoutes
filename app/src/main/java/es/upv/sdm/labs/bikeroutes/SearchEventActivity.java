f2ba6c3d5d86c2aa6dd83c9c3200d5e11bd4f95d
Ellinoora Laaksonen <ellinooralaaksonen@veraw56-91.wi-fi.upv.es>
2016-04-06 18:12:15 +0200

    Activities and responding xml added

---
+++ app/src/main/java/es/upv/sdm/labs/bikeroutes/SearchEventActivity.java
@@ -0,0 +1,13 @@
+package es.upv.sdm.labs.bikeroutes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_event);
+    }
+}
