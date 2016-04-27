e7a9ca1b8d44f345f3c83156b430eec5eb6b4d21
Ellinoora Laaksonen <ellinooralaaksonen@aalto.fi>
2016-04-27 02:38:24 +0200

    adding friends, not sure if working, not checking if friend already exist

--- app/src/main/java/es/upv/sdm/labs/bikeroutes/activities/MyFriendsActivity.java
+++ app/src/main/java/es/upv/sdm/labs/bikeroutes/activities/MyFriendsActivity.java
@@ -1,28 +1,37 @@
 package es.upv.sdm.labs.bikeroutes.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import es.upv.sdm.labs.bikeroutes.R;
 import es.upv.sdm.labs.bikeroutes.adapters.PersonAdapter;
+import es.upv.sdm.labs.bikeroutes.dao.UserDAO;
 import es.upv.sdm.labs.bikeroutes.model.User;
 
 public class MyFriendsActivity extends AppCompatActivity {
 
     ListView friendsListView;
     Context context;
+    User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_friends);
         context = this;
-
+        UserDAO dao = new UserDAO(this);
+        user = dao.findById(PreferenceManager.getDefaultSharedPreferences(this).getInt("user_id",0));
         friendsListView = (ListView) findViewById(R.id.lvMyFriends);
 
         //Log.d("DashboardActivity", "bla");
@@ -45,14 +54,36 @@ public class MyFriendsActivity extends AppCompatActivity {
 
     private void populateFriendsList() {
         //Construct data source
-        ArrayList<User> arrayFriends = User.getUsers();
+        ArrayList<User> arrayFriends = user.getFriends();
 
-        Log.d("MyFriendsActivity", arrayFriends.get(0).toString());
+        if(arrayFriends.size() > 0) {
+            Log.d("MyFriendsActivity", arrayFriends.get(0).toString());
+            //Create the adapter to convert the array to views
+            PersonAdapter adapter = new PersonAdapter(this, arrayFriends);
+            //attach the adapter to the listview
+            friendsListView.setAdapter(adapter);
+        } else {
+            Toast.makeText(MyFriendsActivity.this, "You have no friends", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        //Create the adapter to convert the array to views
-        PersonAdapter adapter = new PersonAdapter(this, arrayFriends);
-        //recentEventsListView = (ListView) findViewById(R.id.lvRecentEvents);
-        //attach the adapter to the listview
-        friendsListView.setAdapter(adapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_friends_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.menuAddFriends:
+                intent = new Intent(this, AddFriendsActivity.class);
+                startActivity(intent);
+                break;
+            case (android.R.id.home):
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
